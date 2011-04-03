b34afd1be61167d38842212dab8047e2e0943fb9
Christian Hammond <chipx86@chipx86.com>
2011-04-03 11:59:54 -0700

    Add basic support for pausing.
    The gameplay will stop. There's no pause screen.

--- foreverend/game.py
+++ foreverend/game.py
@@ -257,6 +257,7 @@ class ForeverEndEngine(object):
     def __init__(self, screen):
         self.screen = screen
         self.clock = pygame.time.Clock()
+        self.paused = False
 
     def run(self):
         self.bg = pygame.Surface(self.screen.get_size()).convert()
@@ -288,22 +289,37 @@ class ForeverEndEngine(object):
             (event.type == KEYDOWN and event.key == K_ESCAPE)):
             pygame.quit()
             return False
+        elif event.type == KEYDOWN and event.key == K_RETURN:
+            if self.paused:
+                self._unpause()
+            else:
+                self._pause()
         else:
             self.player.handle_event(event)
 
         return True
 
+    def _pause(self):
+        self.paused = True
+
+    def _unpause(self):
+        self.paused = False
+
+    def _show_time_periods(self):
+        self._pause()
+
     def _paint(self):
         self.screen.blit(self.bg, (0, 0))
-        self.level.draw(self.screen)
+        self.active_levelset.active_level.draw(self.screen)
         pygame.display.flip()
 
     def _tick(self):
-        self.level.tick()
+        if not self.paused:
+            self.active_levelset.active_level.tick()
 
-        for sprite in [self.player]:
-            if sprite.velocity != (0, 0):
-                sprite.move_by(*sprite.velocity)
+            for sprite in [self.player]:
+                if sprite.velocity != (0, 0):
+                    sprite.move_by(*sprite.velocity)
 
 
 def main():
