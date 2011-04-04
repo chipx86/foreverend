e00da50724062df764199f01b67bbe280992c716
Christian Hammond <chipx86@chipx86.com>
2011-04-04 03:22:30 -0700

    Add setup.py distribution support.
    This gives us the usual setuptools support, plus cx-freeze support.

--- foreverend/resources.py
+++ foreverend/resources.py
@@ -4,6 +4,13 @@ import pygame
 
 image_cache = {}
 
+DATA_PY = os.path.abspath(os.path.dirname(__file__))
+DATA_DIR = os.path.normpath(os.path.join(DATA_PY, '..', 'data'))
+
+# Try to find it in the exe.
+if not os.path.exists(DATA_DIR):
+    DATA_DIR = os.path.normpath(os.path.join(DATA_PY, '..', '..', 'data'))
+
 
 def get_cached_image(name, create_func):
     assert name
@@ -18,8 +25,7 @@ def load_image(name):
     def _load_image_file():
         filename = name + '.png'
 
-        path = os.path.join(os.path.dirname(__file__), 'data',
-                            *filename.split('/'))
+        path = os.path.join(DATA_DIR, *filename.split('/'))
 
         try:
             return pygame.image.load(path)
---
+++ setup.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+import sys
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import find_packages
+
+try:
+    import py2exe
+except:
+    pass
+
+
+PACKAGE_NAME = 'forever_end'
+APP_NAME = 'foreverend'
+VERSION = '0.1'
+
+METADATA = dict(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description='Time has been fractured by a mysterious artifact, and '
+                'only *you* are probe enough to fix it.',
+    author='Christian Hammond',
+    author_email='chipx86@chipx86.com',
+    entry_points={
+        'console_scripts': [
+            'forever_end = foreverend.game:main',
+        ],
+    },
+    install_requires=[
+        'pygame',
+    ],
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Games/Entertainment :: Platforming',
+    ])
+
+cmd = sys.argv[1]
+
+if cmd in ('build_exe', 'install_exe'):
+    from cx_Freeze import setup, Executable
+
+    METADATA['executables'] = [Executable('main.py')]
+
+    #setup(**METADATA)
+
+    exclude_modules = ['tcl', 'tk']
+
+    dist_dir = os.path.join('dist', '%s_%s' % (PACKAGE_NAME, VERSION))
+    data_dir = os.path.join(os.path.dirname(__file__), 'data')
+    os.system("cxfreeze --install-dir='%s' --target-name='%s' "
+              "--exclude-modules=%s main.py" %
+              (dist_dir, APP_NAME, ','.join(exclude_modules)))
+
+    for dirname in exclude_modules:
+        path = os.path.join(dist_dir, dirname)
+
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+    dest_data_dir = os.path.join(dist_dir, 'data')
+
+    if os.path.exists(dest_data_dir):
+        shutil.rmtree(dest_data_dir)
+
+    shutil.copytree(data_dir, dest_data_dir)
+else:
+    from setuptools import setup
+    setup(**METADATA)
