24e6416d6fe805c1f33dda7909da34e270647a4b
Christian Hammond <chipx86@chipx86.com>
2011-04-06 19:04:08 -0700

    Don't import t600ad. It doesn't exist anymore.

--- foreverend/sprites/__init__.py
+++ foreverend/sprites/__init__.py
@@ -5,4 +5,3 @@ from foreverend.sprites.player import *
 from foreverend.sprites.t12000bc import *
 from foreverend.sprites.t1999ad import *
 from foreverend.sprites.t65000000bc import *
-from foreverend.sprites.t600ad import *
